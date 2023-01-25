f3c32333bfd136daf97d5a42c9f14e6c42687388
PatrickVenanzio <123539976+PatrickVenanzio@users.noreply.github.com>
2023-01-25 08:50:34 +0100

    Create registrazione.php

---
+++ registrazione.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="stile.css">
+    </head>
+    <body>
+        
+        <script src="" async defer></script>
+
+        <div class="contenitore">
+            <div class="contenitoreLogin">
+                <div class="riquadroLogin">
+                    <div id="titolo">Registrati</div>
+                    <div id="scritte">
+                        Nome
+                        <input type="text" class="casellaDati" required /><br><br>
+
+                        Cognome
+                        <input type="text" class="casellaDati" /><br><br>
+
+                        User name
+                        <input type="text" class="casellaDati" /><br><br>
+
+                        Inserisci la tua email
+                        <input type="email" class="casellaDati" /><br><br>
+                        
+                        Inserisci la password
+                        <input type="password" class="casellaDati" />
+                        
+                        <div id="scrittePiccole">
+                            <input type="checkbox"> Acconsento al trattamento dei miei dati personali
+                            <br><br>
+                            <input type="checkbox"> Partecipo alla newlestter
+                        </div>
+                        <form action="index.php"> 
+                            <input style="margin-bottom:0" type="submit" class="bottone" value="REGISTRATI" />
+                        </form>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </body>
+</html>  
